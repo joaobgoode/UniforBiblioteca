47254321882b2637f48b50703ab55ec8e39db582
joaobgoode <joaovictorvvm@edu.unifor.br>
2025-10-10 13:11:45 -0300

    feat: tela de configuração

--- app/src/main/java/com/example/uniforbiblioteca/ContatoFragment.kt
+++ app/src/main/java/com/example/uniforbiblioteca/ContatoFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -14,18 +13,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MenuFragment.newInstance] factory method to
+ * Use the [ContatoFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MenuFragment : Fragment() {
+class ContatoFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
-    lateinit var contatoBtn: TextView
-    lateinit var chatBtn: TextView
-    lateinit var configBtn: TextView
-    lateinit var acervoBtn: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,7 +34,7 @@ class MenuFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false)
+        return inflater.inflate(R.layout.fragment_contato, container, false)
     }
 
     companion object {
@@ -50,44 +44,16 @@ class MenuFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MenuFragment.
+         * @return A new instance of fragment ContatoFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MenuFragment().apply {
+            ContatoFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        (activity as? MainActivity)?.changeState("menu")
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        contatoBtn = view.findViewById(R.id.menuContatoBtn)
-        chatBtn = view.findViewById(R.id.menuChatBtn)
-        configBtn = view.findViewById(R.id.menuConfigButton)
-        acervoBtn = view.findViewById(R.id.menuAcervoButton)
-
-        contatoBtn.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.mainFragmentContainer, ContatoFragment::class.java, null)
-                .addToBackStack("menu")
-                .commit()
-        }
-
-        configBtn.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.mainFragmentContainer, ConfigFragment::class.java, null)
-                .addToBackStack("menu")
-                .commit()
-        }
     }
 }
